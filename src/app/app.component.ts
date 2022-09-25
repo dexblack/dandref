3ac4e8753e3fa252c95bff1828c7d4e47748fd26
dex black <3454616+dexblack@users.noreply.github.com>
2022-09-25 12:35:37 +1000

    Initial commit of base angular typescript project with datatable example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dandref';
+  dtOptions: DataTables.Settings = {};
+  posts:any[];
+  
+  constructor(private http: HttpClient) { }
+   
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true
+    };
+   
+    this.http.get('http://jsonplaceholder.typicode.com/posts')
+      .subscribe(posts:any[] => {
+        this.posts = posts;
+    });
+   
+  }  
 }
